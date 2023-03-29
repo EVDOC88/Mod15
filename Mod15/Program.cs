01d67c38dcb78997937d8e15ed1e07d4076aae3f
EVDOC88 <vovan8888@mail.ru>
2023-03-29 20:50:35 +0300

    Добавьте файлы проекта.

---
+++ Mod15/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Mod15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+           };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join(" ", allStudents));
+        }
+
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+
+            return classes.SelectMany(s => s.Students).ToArray();
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
